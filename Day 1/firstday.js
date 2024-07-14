f6589b01cfa3f590c1050062bd3688360dcc2229
Arkajyoti Naskar <86280798+Arka2708@users.noreply.github.com>
2024-07-14 19:30:21 +0000

    day 1 done

---
+++ Day 1/firstday.js
@@ -0,0 +1,43 @@
+//Task 1
+var num = 10
+console.log(num)
+
+//Task 2
+let s = "arka"
+console.log(s)
+
+//Task 3
+const check = true
+console.log(check)
+
+//Task 4
+let n = 7 //Number
+console.log(typeof(n))
+
+let firstname = "Arkajyoti" //String
+console.log(typeof(firstname))
+
+let isPlaced = false //boolean
+console.log(typeof(isPlaced))
+
+let einfo ={
+    id : 1,
+    role : "developer",
+    company :"google"
+} //object
+console.log(typeof(einfo))
+
+const techStack = ["Mongodb", "Expreess.js","React.js", "Node.js"] //array
+console.log(typeof(techStack))// IN JS arrays are objects
+
+//Task 5
+let a = 5
+console.log(a)
+a=10
+console.log(a)
+
+//Task 6
+const b = 99
+console.log(b)
+// b=100 //->this will throw error as "Assignment to constant variable"
+console.log(b)
